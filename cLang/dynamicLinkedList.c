742eb585184a2daac24c572edc8fb7ecc0bbb267
Edmar-Rafael <edmar.djou@gmail.com>
2024-08-27 14:33:54 -0300

    changePosition and listInverter added

--- cLang/dynamicLinkedList.c
+++ cLang/dynamicLinkedList.c
@@ -207,14 +207,15 @@ void printList(Node *list) {
 }
 
 int main() {
-  int choice, val, after;
+  int choice, val, after, pos;
   char option1[] = "0 - Exit\n1 - insert at begin\n2 - Remove\n3 - Print\n4 - insert at end\n";
   char option2[] = "5 - insert at middle\n6 - insert at specific\n7 - Remove at end\n";
-  char option3[] = "8 - Clear list\n\n";
+  char option3[] = "8 - Remove at specific position\n9 - Clear list\n10 - change positions\n";
+  char option4[] = "11 - To invert\n\n";
   Node *remove, *list = NULL;
 
   do {
-    printf("%s%s%s", option1, option2, option3);
+    printf("%s%s%s%s", option1, option2, option3, option4);
     scanf("%d", &choice);
     getchar();
 
@@ -262,6 +263,13 @@ int main() {
       break;
 
       case 8:
+        printf("Type a number to remove: ");
+        scanf("%d", &pos);
+        remove = removeAtspecificPos(&list, pos);
+        remove ? printf("%d removed.\n\n", remove->data) : printf("%d not found.\n\n", pos);
+      break;
+
+      case 9:
         remove = removeAll(&list);
         if(remove) {
           printf("Erased items: ");
@@ -271,8 +279,21 @@ int main() {
         }
       break;
 
+      case 10:
+        printf("Type the first node: ");
+        scanf("%d", &val);
+        printf("Type the node to change with the first: ");
+        scanf("%d", &after);
+        changePlace(&list, val, after);
+      break;
+
+      case 11:
+        list = listInverter(&list);
+        list ? printf("List was inverted.\n\n") : printf("Nothing to invert.\n\n");
+      break;
+
       default:
-        choice != 0 ? printf("All good!\n") : printf("Good bye!\n\n");
+        choice != 0 ? printf("invalid option!\n") : printf("Good bye!\n\n");
     }
   } while(choice != 0);
   
